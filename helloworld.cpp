1bb61d3ab7c3a5cd1706efc392ef04aa816c2442
Fotaly <942637914@qq.com>
2023-07-23 19:02:47 +0800

    modify this cpp file

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main()
 {
+    int a,b;
+    cin >> a >> b;
     cout << "hellowrold!\n";
     cout << "what a beautiful world!!\n";
+    cout << a + b << endl;
     system("pause");
     return 0;
 }
